b39d69e421411dcf13702ce8d928ea85e7f07eac
Antoine James Tournepiche <antoinejt.serveur@gmail.com>
2019-12-09 11:43:54 +0100

    :white_check_mark: 100% coverage of Employe

--- src/testsUnitaires/personnel/TestEmploye.java
+++ src/testsUnitaires/personnel/TestEmploye.java
@@ -5,12 +5,12 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 class TestEmploye {
-
+	private static Ligue ligue = new Ligue("Tir à l'arc");
+	private static Employe employe = ligue.addEmploye("Karlo", "Jean-Eude", "jeaneude.karlo@karlo.com", "KarloIsTheBest");
+	
 	@Test
 	void testEstRoot() 
 	{
-		Ligue ligue = new Ligue("Tir à l'arc");
-		Employe employe = ligue.addEmploye( "Karlo", "Jean-Eude", "jeaneude.karlo@karlo.com", "KarloIsTheBest");
 		assertFalse(employe.estRoot());
 		assertTrue(GestionPersonnel.getGestionPersonnel().getRoot().estRoot());
 	}
@@ -18,6 +18,67 @@ class TestEmploye {
 	@Test
 	void testEstAdmin()
 	{
+		ligue.setAdministrateur(employe);
+		Ligue ligueEscrime = new Ligue("Escrime");
+		assertTrue(employe.estAdmin(ligue));
+		assertFalse(employe.estAdmin(ligueEscrime));
+	}
+	
+	@Test
+	void testCheckPassword()
+	{
+		assertTrue(employe.checkPassword("KarloIsTheBest"));
+		assertFalse(employe.checkPassword("lulz"));
+	}
+	
+	@Test
+	void testMailGetter()
+	{
+		assertEquals(employe.getMail(), "jeaneude.karlo@karlo.com");
+	}
+	
+	@Test
+	void testSetters()
+	{
+		Employe employe = new Employe(ligue, "Abandonné", "Déchet", "poubelle@trash.org", "ptdr");
 		
+		employe.setMail("hello@world.org");
+		assertEquals(employe.getMail(), "hello@world.org");
+		
+		employe.setNom("Vilo");
+		assertEquals(employe.getNom(), "Vilo");
+		
+		employe.setPrenom("Lucien");
+		assertEquals(employe.getPrenom(), "Lucien");
+		
+		employe.setPassword("MotDePasse");
+		assertTrue(employe.checkPassword("MotDePasse"));
+	}
+	
+	@Test
+	void testToString()
+	{
+		Employe root = GestionPersonnel.getGestionPersonnel().getRoot();
+		assertEquals(employe.toString(), "Karlo Jean-Eude jeaneude.karlo@karlo.com (Tir à l'arc)");
+		assertEquals(root.toString(), root.getNom() + " " + root.getPrenom() + " " + root.getMail() + " (super-utilisateur)");
+	}
+	
+	// TODO Virer les effets de bords!!
+	@Test
+	void testRemove()
+	{
+		Employe employe = new Employe(ligue, "Paskal", "Lucien", "letesteurunitaire@codeur.org", "TesterCEstDouterCorrigerCEstAbdiquer");
+		employe.remove();
+		assertFalse(ligue.getEmployes().contains(employe));
+		
+		Employe root = GestionPersonnel.getGestionPersonnel().getRoot();
+		
+		Employe admin = ligue.addEmploye("Charles", "Guy", "guycharles@cookie.org", "VivLéKouki");
+		ligue.setAdministrateur(admin);
+		admin.remove();
+		assertFalse(ligue.getEmployes().contains(admin));
+		assertEquals(ligue.getAdministrateur(), root);
+		
+		assertThrows(ImpossibleDeSupprimerRoot.class, () -> { root.remove(); });
 	}
 }
